72938ed8d11d26e98a2bd15857e96182b5351000
Jinglan-design <3056990389@qq.com>
2026-05-14 20:51:37 +0800

    Update and rename mainwindow.h2.0 to mainwindow.h
    增加音效相关代码

--- mainwindow.h
+++ mainwindow.h
@@ -4,8 +4,9 @@
 #include <QMainWindow>
 #include <QLabel>
 #include <QPushButton>
-#include <QComboBox>
-#include <QDialog>
+#include<QComboBox>
+#include <QCheckBox>
+#include<QDialog>
 #include "minefield.h"
 
 class MainWindow : public QMainWindow
@@ -22,16 +23,18 @@ private slots:
     void onMineCountChanged(int count);
     void onDifficultyChanged(int index);
     void onCustomGame();
+    void onSoundToggled(bool checked);
 
 private:
     void setupUI();
-    void startGame(int rows, int cols, int mineCount);
+    void startGame(int rows,int cols,int mineCount);
 
     MineField *m_mineField;
     QLabel *m_mineCountLabel;
     QPushButton *m_newGameBtn;
     QComboBox *m_difficultyCombo;
     QPushButton *m_customBtn;
+    QCheckBox *m_soundCheckBox;
 };
 
 #endif
